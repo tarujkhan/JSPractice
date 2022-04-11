efc97402abe08afb80c9b527b8b605802107297d
tarujkhan <tarujkhan@yahoo.com>
2022-04-11 10:47:59 -0500

    Working on my first DOM project

---
+++ intro-to-dom/app1.js
@@ -0,0 +1,7 @@
+alert("Hello")
+const p1Button = document.querySelector('#p1Button')
+const p2Button = document.querySelector('#p2Button')
+
+p1Button.addEventListener('clicked', function(){
+    alert("clicked")
+})
