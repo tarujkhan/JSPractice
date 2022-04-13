b1e1b51da193c2eeed5d2022ebe2493ffd9f1df9
tarujkhan <tarujkhan@yahoo.com>
2022-04-13 16:20:04 -0500

    My first DOM project works. Now will create a second one.

--- intro-to-dom/app.js
+++ intro-to-dom/app.js
@@ -1,11 +1,11 @@
 
 
-alert("Hello")
+
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#reset')
-const p1Display = document.querySelector('p1Display');
-const p2Display = document.querySelector('p2Display');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
 const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
@@ -16,7 +16,7 @@ let isGameOver = false;
 p1Button.addEventListener('click', function(){
     if(!isGameOver){
     p1Score += 1;
-    if (p1Score = winningScore) 
+    if (p1Score === winningScore) { 
        isGameOver = true;     
     }
     p1Display.textContent = p1Score;
@@ -26,8 +26,8 @@ p1Button.addEventListener('click', function(){
 p2Button.addEventListener('click', function(){
     if(!isGameOver){
     p2Score += 1;
-    if (p2Score = winningScore) 
-       isGameOver = true;     
+    if (p2Score === winningScore) {
+       isGameOver = true;    
     }
     p2Display.textContent = p1Score;
     }
